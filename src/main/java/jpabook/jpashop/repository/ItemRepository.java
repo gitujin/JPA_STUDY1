a10827d927553145711fd0804e10c3ecfd90f251
gitujin <syj991114@gmail.com>
2022-08-10 19:39:39 +0900

    상품 주문, 검색, 취소

--- src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -1,17 +1,17 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.item.Item;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
 public class ItemRepository {
 
-    private final EntityManager em;
+    @PersistenceContext
+    EntityManager em;
 
     public void save(Item item){
         if(item.getId() == null){
